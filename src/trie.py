398049382b1310493f77ae58ef79e13c2483957c
valrus <imccowan@gmail.com>
2017-03-03 11:34:30 -0800

    Clean up heap adding logic a bit, remove debug print

--- src/trie.py
+++ src/trie.py
@@ -17,20 +17,16 @@ class TrieNode(object):
             next_node = curr_node.nexts[word]
             curr_node = next_node
         curr_node.count += 1
-        if curr_node.count > 1:
-            print('Duplicate: ', phrase)
-            return True
-        return False
+        return curr_node.count > 1
 
     def find_top(self, k, heap, phrase=None):
         phrase = phrase or []
+        heap = heap or MaxHeap(size=k)
+        skip_this = (self.count == 0 or len(phrase) == 1)
         if self.nexts:
-            found_longer = self.count == 0
             for next_word, next_node in self.nexts.items():
                 if next_node.count > 1:
                     next_node.find_top(k, heap, phrase + [next_word])
-                found_longer |= (next_node.count >= self.count)
-            if not found_longer:
-                heap.add(self.count, tuple(phrase))
-        else:
+                skip_this |= (next_node.count >= self.count)
+        if not skip_this:
             heap.add(self.count, tuple(phrase))
